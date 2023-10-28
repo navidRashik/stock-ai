9eacc56396f1ee22a234542b0f3c8610fc37b2f6
navidRashik <navid.rashik@gmail.com>
2023-10-28 22:27:57 +0600

    added admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Portfolio, Transaction, Stock, TickerUpdate, Ticker, Position
 
 # Register your models here.
+admin.site.register(Portfolio)
+admin.site.register(Transaction)
+admin.site.register(Stock)
+admin.site.register(TickerUpdate)
+admin.site.register(Ticker)
+admin.site.register(Position)
